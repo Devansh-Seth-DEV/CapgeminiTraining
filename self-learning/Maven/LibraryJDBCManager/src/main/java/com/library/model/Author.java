8fce997a5c70a7e9efba59e46ba45becf0f22968
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2026-03-07 09:39:48 +0530

    Add files via upload

---
+++ self-learning/Maven/LibraryJDBCManager/src/main/java/com/library/model/Author.java
@@ -0,0 +1,42 @@
+package com.library.model;
+
+public class Author {
+	private int authorId;
+	private String firstName;
+	private String lastName;
+	
+	public Author(int authorId, String firstName, String lastName) {
+		this.authorId = authorId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getAuthorId() {
+		return authorId;
+	}
+
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Author [authorId=%s, firstName=%s, lastName=%s]", authorId, firstName, lastName);
+	}
+}
