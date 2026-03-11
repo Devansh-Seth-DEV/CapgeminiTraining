878cfd949ef8d22f852199bfab83edddc9686cd5
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2026-03-11 13:40:58 +0530

    Add files via upload

--- concepts/hibernate/hibernate-company/src/main/java/com/gal/dao/VendorDao.java
+++ concepts/hibernate/hibernate-company/src/main/java/com/gal/dao/VendorDao.java
@@ -1,19 +1,19 @@
 package com.gal.dao;
 
 import com.gal.EntityManagerFactoryProvider;
-import com.gal.model.Item;
+import com.gal.model.Vendor;
 
 import jakarta.persistence.EntityManager;
 
-public class ItemDao {
+public class VendorDao {
 	private static EntityManager em = null;
 	
 	static {
 		em = EntityManagerFactoryProvider.getEntityManager();
 	}
 	
-	public Item addItem(Item item) {
-		em.persist(item);
-		return item;
+	public Vendor addVendor(Vendor vendor) {
+		em.persist(vendor);
+		return vendor;
 	}
 }
