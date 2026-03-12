9d929f588f50dc044b3e63cebcab303a14d8cb74
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2026-03-12 14:26:58 +0530

    Add files via upload

--- concepts/spring/SpringBeanDemo/src/main/java/com/gal/algo/MyCalc.java
+++ concepts/spring/SpringBeanDemo/src/main/java/com/gal/algo/MyCalc.java
@@ -1,6 +1,10 @@
 package com.gal.algo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class MyCalc implements Calc {
@@ -8,5 +12,14 @@ public class MyCalc implements Calc {
 	public int add(int a, int b) {
 		return a+b;
 	}
-
+	
+	@PostConstruct
+	public void onInit() {
+		System.out.println("Initialization method for DBConnect, WebSockets open ..");
+	}
+	
+	@PreDestroy
+	public void onDestroy() {
+		System.out.println("Destroy method for closing connection etc...");
+	}
 }
